95548aad96e9c550667d70be8ada6887927b194d
ISAWASHUN <syun20000616@icloud.com>
2024-11-06 23:51:26 +0900

    add route

---
+++ src/routes/todoRoutes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { 
+  createTodo,
+  deleteTodo, 
+  getAllTodos, 
+  getTodo, 
+  updateTodo 
+} from "../controllers/todoController";
+
+const router = Router();
+
+router.get("/", getAllTodos);
+router.get("/:id", getTodo);
+router.post("/", createTodo);
+router.put("/:id", updateTodo);
+router.delete("/:id", deleteTodo)
+
+export default router;
